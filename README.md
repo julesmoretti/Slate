874adb23760f0ab72b2efaa2769650bb220375b0
Jules Moretti <julesmoretti@me.com>
2016-07-25 09:24:09 -0700

    added current settings

---
+++ README.md
@@ -0,0 +1,20 @@
+#CUSTOM SLATE SCRIPT
+
+##INSTALLATION
+```
+cd ~/
+git clone "repo address" slate
+touch .slate.js
+```
+
+Add to the `.slate.js` file the following:
+
+`var success = slate.source("~/slate/slate.js");`
+
+And then install and run:
+`cd /Applications && curl http://www.ninjamonkeysoftware.com/slate/versions/slate-latest.tar.gz | tar -xz`
+
+Based on installation instructions from [Slate.js](https://github.com/jigish/slate/)
+
+##More Slate.js References:
+https://github.com/jigish/slate/wiki/JavaScript-Configs
---
+++ slate.js
@@ -0,0 +1,155 @@
+S.log("START");
+// Configs
+S.cfga({
+	"defaultToCurrentScreen" : true,
+	"secondsBetweenRepeat" : 0.1,
+	"checkDefaultsOnLoad" : true,
+	"focusCheckWidthMax" : 3000,
+	"orderScreensLeftToRight" : true
+});
+
+// Monitors
+var monTbolt  = "2560x1600";
+var monLaptop = "1920x1200";
+
+// Operations
+var lapFull = S.op("move", {
+	"screen" : monLaptop,
+	"x" : "screenOriginX",
+	"y" : "screenOriginY",
+	"width" : "screenSizeX",
+	"height" : "screenSizeY"
+});
+var lapChat = S.op("corner", {
+	"screen" : monLaptop,
+	"direction" : "top-left",
+	"width" : "screenSizeX/9",
+	"height" : "screenSizeY"
+});
+var lapMain = lapChat.dup({ "direction" : "top-right", "width" : "8*screenSizeX/9" });
+var tboltFull = S.op("move", {
+	"screen" : monTbolt,
+	"x" : "screenOriginX",
+	"y" : "screenOriginY",
+	"width" : "screenSizeX",
+	"height" : "screenSizeY"
+});
+var tboltBig = S.op("move", {
+	"screen" : monTbolt,
+	"x" : "screenOriginX+screenSizeX/3",
+	"y" : "screenOriginY",
+	"width" : "screenSizeX*2/3",
+	"height" : "screenSizeY",
+});
+var tboltLeft = tboltFull.dup({ "width" : "screenSizeX/3" });
+var tboltMid = tboltLeft.dup({ "x" : "screenOriginX+screenSizeX/3" });
+var tboltRight = tboltLeft.dup({ "x" : "screenOriginX+(screenSizeX*2/3)" });
+var tboltLeftTop = tboltLeft.dup({ "height" : "screenSizeY/2" });
+var tboltLeftBot = tboltLeftTop.dup({ "y" : "screenOriginY+screenSizeY/2" });
+var tboltMidTop = tboltMid.dup({ "height" : "screenSizeY/2" });
+var tboltMidBot = tboltMidTop.dup({ "y" : "screenOriginY+screenSizeY/2" });
+var tboltRightTop = tboltRight.dup({ "height" : "screenSizeY/2" });
+var tboltRightBot = tboltRightTop.dup({ "y" : "screenOriginY+screenSizeY/2" });
+
+var halfLeft = tboltFull.dup({ "width" : "screenSizeX/2" });
+var quarterTopLeft = halfLeft.dup({ "height" : "screenSizeY/2" });
+var quarterTopRight = quarterTopLeft.dup({ "x" : "screenOriginX+(screenSizeX*1/2)" });
+var quarterBottomLeft = quarterTopLeft.dup({ "y" : "screenOriginY+screenSizeY/2" });
+var quarterBottomRight = quarterTopRight.dup({ "y" : "screenOriginY+screenSizeY/2" });
+
+// Batch bind everything. Less typing.
+S.bnda({
+
+	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ DONE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+
+	// FULL
+	// Resize Screen AKA full screen
+	// NOTE: some of these may *not* work if you have not removed the expose/spaces/mission control bindings
+	"up:shift;ctrl;alt;cmd" : S.op("push", { "direction" : "left", "style" : "bar-resize:screenSizeX/1" }),
+	// Layout Bindings
+	"padEnter:ctrl;alt" : tboltFull,
+
+	// HALVES
+	// Push Bindings AKA halves
+	// NOTE: some of these may *not* work if you have not removed the expose/spaces/mission control bindings
+	"-:ctrl;alt;cmd" : quarterTopLeft,
+	"=:ctrl;alt;cmd" : quarterTopRight,
+	"[:ctrl;alt;cmd" : quarterBottomLeft,
+	"]:ctrl;alt;cmd" : quarterBottomRight,
+
+	"right:ctrl;alt;cmd" : S.op("push", { "direction" : "right", "style" : "bar-resize:screenSizeX/2" }),
+	"left:ctrl;alt;cmd" : S.op("push", { "direction" : "left", "style" : "bar-resize:screenSizeX/2" }),
+	"up:ctrl;alt;cmd" : S.op("push", { "direction" : "up", "style" : "bar-resize:screenSizeY/2" }),
+	"down:ctrl;alt;cmd" : S.op("push", { "direction" : "down", "style" : "bar-resize:screenSizeY/2" }),
+
+	// THIRDS KEYBOARD
+	"8:ctrl;alt" : tboltLeft,
+	"9:ctrl;alt" : tboltMid,
+	"0:ctrl;alt" : tboltRight,
+
+	// THIRDS PADS
+	"pad7:ctrl;alt" : tboltLeft,
+	"pad8:ctrl;alt" : tboltMid,
+	"pad9:ctrl;alt" : tboltRight,
+
+	// GRID 3x2 SELECTOR KEYBOARD
+	"k:ctrl;alt" : tboltLeftBot,
+	"l:ctrl;alt" : tboltMidBot,
+	";:ctrl;alt" : tboltRightBot,
+	"i:ctrl;alt" : tboltLeftTop,
+	"o:ctrl;alt" : tboltMidTop,
+	"p:ctrl;alt" : tboltRightTop,
+
+
+	// GRID 3x2 SELECTOR PAD
+	"pad1:ctrl;alt" : tboltLeftBot,
+	"pad2:ctrl;alt" : tboltMidBot,
+	"pad3:ctrl;alt" : tboltRightBot,
+	"pad4:ctrl;alt" : tboltLeftTop,
+	"pad5:ctrl;alt" : tboltMidTop,
+	"pad6:ctrl;alt" : tboltRightTop,
+
+	// CHAT VERSION KEYBOARD
+	// Basic Location Bindings
+	"-:ctrl;alt" : lapChat,
+	"=:ctrl;alt" : lapMain,
+
+	// CHAT VERSION PAD
+	// Basic Location Bindings
+	"pad0:ctrl;alt" : lapChat,
+	"pad.:ctrl;alt" : lapMain,
+
+	// Grid AKA draw grid
+	"esc:ctrl" : S.op("grid"),
+
+	// Resize Bindings
+	// NOTE: some of these may *not* work if you have not removed the expose/spaces/mission control bindings
+	"right:shift;alt" : S.op("resize", { "width" : "+10%", "height" : "+0" }),
+	"left:shift;alt" : S.op("resize", { "width" : "-10%", "height" : "+0" }),
+	"up:shift;alt" : S.op("resize", { "width" : "+0", "height" : "-10%" }),
+	"down:shift;alt" : S.op("resize", { "width" : "+0", "height" : "+10%" }),
+
+	"right:shift;ctrl" : S.op("resize", { "width" : "-10%", "height" : "+0", "anchor" : "bottom-right" }),
+	"left:shift;ctrl" : S.op("resize", { "width" : "+10%", "height" : "+0", "anchor" : "bottom-right" }),
+	"up:shift;ctrl" : S.op("resize", { "width" : "+0", "height" : "+10%", "anchor" : "bottom-right" }),
+	"down:shift;ctrl" : S.op("resize", { "width" : "+0", "height" : "-10%", "anchor" : "bottom-right" }),
+
+	// MOVE WINDOW
+	// Nudge Bindings AKA move around
+	// NOTE: some of these may *not* work if you have not removed the expose/spaces/mission control bindings
+	"right:alt" : S.op("nudge", { "x" : "+10%", "y" : "+0" }),
+	"left:alt" : S.op("nudge", { "x" : "-10%", "y" : "+0" }),
+	"up:alt" : S.op("nudge", { "x" : "+0", "y" : "-10%" }),
+	"down:alt" : S.op("nudge", { "x" : "+0", "y" : "+10%" }),
+
+	// MOVE TO SCREEN
+	// Throw Bindings AKA send selection to direction screen
+	// NOTE: some of these may *not* work if you have not removed the expose/spaces/mission control bindings
+	"right:ctrl;alt" : S.op("throw", { "screen" : "right", "width" : "screenSizeX", "height" : "screenSizeY" }),
+	"left:ctrl;alt" : S.op("throw", { "screen" : "left", "width" : "screenSizeX", "height" : "screenSizeY" }),
+	"up:ctrl;alt" : S.op("throw", { "screen" : "up", "width" : "screenSizeX", "height" : "screenSizeY" }),
+	"down:ctrl;alt" : S.op("throw", { "screen" : "down", "width" : "screenSizeX", "height" : "screenSizeY" })
+
+});
+
+S.log("END")
